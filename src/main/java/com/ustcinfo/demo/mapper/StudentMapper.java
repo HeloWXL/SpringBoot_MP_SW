70a849d634d9569ea691c1b9bcceaff2baf5e8f2
王咸林 <756316064@qq.com>
2019-10-22 17:19:45 +0800

    代码修改

--- src/main/java/com/ustcinfo/demo/mapper/StudentMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ustcinfo.demo.mapper;
-
-import com.baomidou.mybatisplus.mapper.BaseMapper;
-import com.ustcinfo.demo.model.Student;
-
-public interface StudentMapper extends BaseMapper<Student> {
-
-}
